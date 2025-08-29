c50c10abc4b77837c3988bb07a24ef9a8783c2ec
Fariel <masdar5769@gmail.com>
2025-08-29 19:55:51 +0700

    Create Praktikum1.java

---
+++ Praktikum 1 /Praktikum1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum_1;
+
+/**
+ *
+ * @author Fariel
+ */
+
+public class Praktikum_1 {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Muhammad Fariel Gifari");
+        System.out.println("Nim saya 2407127428");
+    }
+}
