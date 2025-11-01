b93692f95c9ccfcd768f34f97d8f9f6a0b78a2fe
naufaalyanafisa <alyanafisanaufa@gmail.com>
2025-11-01 16:59:30 +0700

    Add files via upload

---
+++ praktikum_9/Praktikum_9.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikum_9;
+
+/**
+ *
+ * @author M-SI
+ */
+public class Praktikum_9 {
+    public static void main(String[] args) {
+        mainJFrame frame = new mainJFrame();
+        frame.changeMainPanel(new TambahDataPanel());
+        frame.setVisible(true);
+    }
+}
